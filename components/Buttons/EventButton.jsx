dc5df69af7846c476f60958419a64ab25d17e713
Saka <sakamoshood@ymail.com>
2024-03-14 13:34:16 -0400

    added custom event button
    removed refresh and signin button

---
+++ components/Buttons/EventButton.jsx
@@ -0,0 +1,33 @@
+// EventButton.js
+import React from "react";
+import styles from "../../styles/Button.module.css";
+
+// You can extend this component to accept more props as needed
+const EventButton = ({
+  children,
+  onClick,
+  color = "primary", // Default color
+  size = "medium", // Default size
+  disabled = false,
+  loading = false,
+  ...props
+}) => {
+  const buttonClasses = `
+    ${styles.custombutton}
+    ${styles[color]}
+    ${styles[size]}
+    ${disabled ? styles.disabled : ""}
+    ${loading ? styles.loading : ""}
+  `;
+  return (
+    <button
+      onClick={onClick}
+      className={buttonClasses} // Default class merged with custom classes passed as prop
+      {...props} // Spread any additional props
+    >
+      {loading ? "Loading" : children}
+    </button>
+  );
+};
+
+export default EventButton;
